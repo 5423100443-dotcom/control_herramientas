668f7e48ae60aa387a2f19d2bf1e0d2c9f37d0e4
5423100443-dotcom <5423100443@utnuevolaredo.edu.mx>
2026-03-04 21:17:17 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -205,7 +205,7 @@ poner_fondo()
 # =========================
 
 st.title("🏭 Sistema de Control de Herramientas CNC")
-
+st_autorefresh(interval=5000, key="refresh")
 rol = st.session_state["rol"]
 
 # =========================
@@ -523,6 +523,7 @@ if rol in ["toolcrib","supervisor"]:
  
 
    
+
 
 
 
