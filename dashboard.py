3f58156d67550249a3c5bf10f35b8ef60996ecc2
5423100443-dotcom <5423100443@utnuevolaredo.edu.mx>
2026-03-05 19:02:48 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -232,7 +232,7 @@ if rol == "operador":
 elif rol == "toolcrib":
 
     tab_solicitudes, tab_dashboard = st.tabs([
-        "📦 Solicitudes",
+        "📦 Tool Crib",
         "📊 Dashboard"
     ])
 
@@ -283,22 +283,25 @@ with tab_dashboard:
     # verificar si hay filtros
     if mes == "Seleccionar" and maquina == "Seleccionar" and empleado == "Seleccionar":
         st.info("Selecciona al menos un filtro para mostrar información.")
+        st.stop()
 
-    else:
-    
-        # aplicar filtros
-        if mes != "Seleccionar":
-            df_filtrado = df_filtrado[df_filtrado["mes"] == mes]
+     # aplicar filtros
+    if mes != "Seleccionar":
+        df_filtrado = df_filtrado[df_filtrado["mes"] == mes]
+            
+    if maquina != "Seleccionar":
+        df_filtrado = df_filtrado[df_filtrado["maquina"] == maquina]
+            
+    if empleado != "Seleccionar":
+        df_filtrado = df_filtrado[df_filtrado["empleado"] == empleado]
         
-        if maquina != "Seleccionar":
-            df_filtrado = df_filtrado[df_filtrado["maquina"] == maquina]
-        
-        if empleado != "Seleccionar":
-            df_filtrado = df_filtrado[df_filtrado["empleado"] == empleado]
+
     
+           
     # verificar si quedó vacío
     if df_filtrado.empty:
         st.warning("No hay datos con los filtros seleccionados.")
+        st.stop()
 
     # =========================
     # KPIs
@@ -559,6 +562,7 @@ if rol in ["toolcrib","supervisor"]:
  
 
    
+
 
 
 
