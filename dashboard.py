42fe2f4e3224a696e6a5ee764e1c9c3f34554696
5423100443-dotcom <5423100443@utnuevolaredo.edu.mx>
2026-03-05 21:09:17 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -295,6 +295,7 @@ with tab_dashboard:
     # verificar si hay filtros
     if mes == "Seleccionar" and maquina == "Seleccionar" and empleado == "Seleccionar":
         st.warning("Selecciona al menos un filtro para mostrar información.")
+        mostrar_dashboard = False
 
     else:
         
@@ -576,6 +577,7 @@ if rol in ["toolcrib","supervisor"]:
  
 
    
+
 
 
 
