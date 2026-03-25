59926885e6e0d05c63d3327b258a7a4a27817639
5423100443-dotcom <5423100443@utnuevolaredo.edu.mx>
2026-03-24 19:11:08 -0500

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -509,7 +509,7 @@ with tab_dashboard:
         
             fig_cambios = px.bar(
                 df_cambios,
-                x="herramienta",
+                x="herramienta_maquina",
                 y="cantidad_cambios",
                 text="cantidad_cambios",
                 color="maquina",
@@ -556,7 +556,7 @@ with tab_dashboard:
         if not df_gasto.empty:
             fig_gasto = px.bar(
                 df_gasto,
-                x="herramienta",
+                x="herramienta_maquina",
                 y="precio",
                 text="precio",
                 color="maquina",
